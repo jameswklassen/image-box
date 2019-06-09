bc9fac735757c185603872d9be97ce918c6fd41b
James Klassen <hello@jamesklassen.ca>
2019-06-08 20:21:40 -0500

    Adding firebase environment variables

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -2,25 +2,25 @@ import * as firebase from 'firebase';
 
 // Firebase configuration
 var firebaseConfig = {
-  apiKey: "AIzaSyAPEl-hY5NNTQjtA-0vFdaOVVx9AlNGz0Y",
-  authDomain: "image-box-16305.firebaseapp.com",
-  databaseURL: "https://image-box-16305.firebaseio.com",
-  projectId: "image-box-16305",
-  storageBucket: "gs://image-box-16305.appspot.com/",
-    messagingSenderId: "62527557058",
-    appId: "1:62527557058:web:f243e5b10e7531c1"
-  };
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
+};
 
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  
-  const storage = firebase.storage();
-  const provider = new firebase.auth.GoogleAuthProvider();
-  const db = firebase.firestore()
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
-  export {
-      firebase,
-      storage,
-      provider,
-      db
-  }
+const storage = firebase.storage();
+const provider = new firebase.auth.GoogleAuthProvider();
+const db = firebase.firestore()
+
+export {
+    firebase,
+    storage,
+    provider,
+    db
+}
